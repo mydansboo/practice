df8faafaa75d140834ad07cde5dfd7ee19633de1
Daniel <daniellewis777@gmail.com>
2019-11-30 19:06:44 +0000

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'sian';
+
+export class AppComponent implements OnInit {
+
+  myNumber = 11
+
+  ngOnInit() {
+    let myNum: any = localStorage.getItem('a very special number to never ever forget')
+    myNum = parseInt(myNum, 10)
+    this.myNumber = myNum
+  }
+
+  doIt() {
+    this.myNumber = this.myNumber + 11
+    localStorage.setItem('a very special number to never ever forget', this.myNumber.toString())
+  }
 }
